48f5d647e8bb75e63759cf8bb16a360de899f48b
Stefan Formentano <StefanFormentano@gmail.com>
2018-12-08 11:22:16 -0600

    clean up

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// for flipping the card front to back
 const addWeapon = document.querySelector('.addWeapon')
 const doneWeapon = document.querySelector('.doneWeapon')
 const cardPosition = document.querySelector('.cardPosition')
@@ -13,8 +14,23 @@ flip = () => {
 		cardPosition.classList.remove('cardPosition1');
 		cardPosition.classList.add('cardPosition');
 		int = true;
-	}	
+	}
 }
 
 doneWeapon.addEventListener('click', flip)
 addWeapon.addEventListener('click', flip)
+
+// copying block and pasting. creates new card
+const newCard = document.getElementById('newCard')
+
+let cycle = 0;
+
+newBlock = () => {
+	var div = document.createElement('div');
+	div.setAttribute('class', 'cardContainer')
+	div.innerHTML = document.getElementById('block').innerHTML;
+	document.getElementById('target').appendChild(div);
+	cycle += 1;
+}
+
+newCard.addEventListener('click', newBlock)
